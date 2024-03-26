40b22592abeff4b87a074543c6c881ecc16a3cb0
nikonishikawa <161550990+nikonishikawa@users.noreply.github.com>
2024-03-26 09:35:40 +0800

    inital changes

--- Canteen/src/app/app.component.ts
+++ Canteen/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { RouterModule } from '@angular/router';
-import { SignUpComponent } from "./components/users/sign-up/sign-up.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, SignUpComponent]
+    imports: []
 })
 export class AppComponent {
   title = 'Canteen';
